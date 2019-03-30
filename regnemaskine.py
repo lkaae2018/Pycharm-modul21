304b257bdb5b5c34603128dabf10cd15ff730a09
lkaae2018 <lak@djhhadsten.dk>
2019-03-30 10:59:07 +0100

    Modul python

---
+++ regnemaskine.py
@@ -0,0 +1,9 @@
+import Lkaae_modul
+
+Lkaae_modul.lkaae_test(500)
+Lkaae_modul.lkaae_test2(50)
+Lkaae_modul.lkaae_test3(50)
+Lkaae_modul.lkaae_test(23434)
+Lkaae_modul.lkaae_test3(67865)
+Lkaae_modul.lkaae_test(-65)
+Lkaae_modul.lkaae_test2(1236)
